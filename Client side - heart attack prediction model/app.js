871bff52f9174f50129c9e767af17fa55ca5007d
ahmadml <ahmad_msalha@hotmail.com>
2022-08-15 22:06:24 +0300

    update

--- Client side - heart attack prediction model/app.js
+++ Client side - heart attack prediction model/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 var server = require('http').createServer(app);
-const port = 6064;
+const port = process.env.PORT || 8080;
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
@@ -10,4 +10,4 @@ app.get('/', (req, res) => {
     res.render("frontend")
   })
 
-  server.listen(port, () => console.log(`Listening Socket on http://localhost:6064`));
+  server.listen(port, () => console.log(`Listening...`));
